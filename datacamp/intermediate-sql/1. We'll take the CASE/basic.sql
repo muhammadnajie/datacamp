cf6cc3ebd557fae64260c5d832ef95bce57983ab
Muhammad Najie <muhammadalnajie1@gmail.com>
2021-10-23 19:46:56 +0700

    initial commit

---
+++ datacamp/intermediate-sql/1. We'll take the CASE/basic.sql
@@ -0,0 +1,18 @@
+-- 1/2
+SELECT
+	-- Select the team long name and team API id
+	team_long_name,
+	team_api_id
+FROM teams_germany
+-- Only include FC Schalke 04 and FC Bayern Munich
+WHERE team_long_name IN ('FC Bayern Munich', 'FC Schalke 04');
+
+-- 2/2
+SELECT 
+	CASE WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
+             WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+             ELSE 'Other' 
+        END AS home_team,
+	COUNT(id) AS total_matches
+FROM matches_germany
+GROUP BY home_team;
