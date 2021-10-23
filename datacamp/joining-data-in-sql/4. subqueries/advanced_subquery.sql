7f783ddc5dbb5af62dd0b6e81997c72f92ecdf65
Muhammad Najie <muhammadalnajie1@gmail.com>
2021-10-23 17:09:27 +0700

    advanced subquery

--- datacamp/joining-data-in-sql/4. subqueries/advanced_subquery.sql
+++ datacamp/joining-data-in-sql/4. subqueries/advanced_subquery.sql
@@ -1,8 +1,33 @@
-SELECT countries.continent,
-       (SELECT MAX(inflation_rate)
-        FROM economies
-        WHERE year = 2015
-        AND countries.code = economies.code)
-FROM countries,
-     (SELECT DISTINCT continent FROM countries) AS continents
-WHERE countries.continent = continents.continent;
+-- 1/3
+-- SELECT countries.name, countries.continent, economies.inflation_rate
+-- FROM countries
+-- INNER JOIN economies
+-- USING(code)
+-- WHERE year = 2015;
+
+-- 2/3
+-- SELECT MAX(inflation_rate) AS max_inf
+--   FROM (
+--       SELECT name, continent, inflation_rate
+--       FROM countries
+--       INNER JOIN economies
+--       USING (code)
+--       WHERE year = 2015) AS subquery
+-- GROUP BY continent;
+
+-- 3/3
+SELECT name, continent, inflation_rate
+    FROM countries
+    INNER JOIN economies
+    ON countries.code = economies.code
+    WHERE year = 2015
+      AND inflation_rate IN (
+          SELECT MAX(inflation_rate) AS max_inf
+          FROM (
+              SELECT name, continent, inflation_rate
+              FROM countries
+              INNER JOIN economies
+              ON countries.code = economies.code
+              WHERE year = 2015) AS subquery
+          GROUP BY continent);
+
